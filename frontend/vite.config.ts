ed2f9cc4bf581bb85dcf5f42ec4bc85f22a3a6e5
Rambat Ungu Aryati <rambatungu.arya@student.uns.ac.id>
2026-06-23 07:42:21 +0700

    new

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/api/v1/auth": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true,
       },
       "/api/v1/admin": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true,
       },
       "/api/v1/profiles": {
