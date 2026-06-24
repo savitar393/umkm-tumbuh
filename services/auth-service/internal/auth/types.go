d36fb60ad08f8402c4da405bf970d224bc72b280
savitar393 <rafahtaqy1234@gmail.com>
2026-06-24 23:29:45 +0700

    Add password reset request DTOs

--- services/auth-service/internal/auth/types.go
+++ services/auth-service/internal/auth/types.go
@@ -8,3 +8,13 @@ type ConfirmEmailVerificationRequest struct {
 	Email string `json:"email"`
 	Code  string `json:"code"`
 }
+
+type RequestPasswordResetRequest struct {
+	Email string `json:"email"`
+}
+
+type ResetPasswordRequest struct {
+	Email       string `json:"email"`
+	Code        string `json:"code"`
+	NewPassword string `json:"new_password"`
+}
